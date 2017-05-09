27ed870c4686fe2564bfe6fc80e84c88401d4d88
Henry Jackson <jacksonhg95@gmail.com>
2017-05-09 13:18:07 +0100

    Fixed MathFunction::ValInDomain()

--- tools/mathfunction.hpp
+++ tools/mathfunction.hpp
@@ -29,12 +29,13 @@ public:
 };
 
 
-
+// Return false if value is not in the domain of the function, true otherwise
 bool MathFunction::ValInDomain(double val) {
     if(!restricted_domain) return true;
     else {
         if(val < m_domain[0] || val > m_domain[1]) return false;
     }
+    return true;
 }
 
 
