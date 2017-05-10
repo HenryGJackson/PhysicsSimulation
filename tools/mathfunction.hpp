217f49f51aa92da54061086f9f80014a61cc8ddc
Henry Jackson <jacksonhg95@gmail.com>
2017-05-11 00:23:02 +0100

    Added print and evaluate virtual functions

--- tools/mathfunction.hpp
+++ tools/mathfunction.hpp
@@ -12,6 +12,7 @@ class MathFunction{
 
 private:
     bool restricted_domain;
+    // bool m_differentiable = true;
     vector<double> m_domain;
     string variable;
 
@@ -19,16 +20,24 @@ public:
     MathFunction(string str) : restricted_domain(false), variable(str) {}
     MathFunction(string str, vector<double> dom)
       : restricted_domain(true), variable(str) {
-          try {if(dom.size() != 2) throw 1}
+          try {if(dom.size() != 2) throw 1;}
           catch(int e) {
               std::cout << "Domain must be a vector of length 2: dom(min, max)\n";
               exit(EXIT_FAILURE);
           }
       }
     bool ValInDomain(double val);
+    string GetTag();
+    virtual double Evaluate(double val);
+    virtual void PrintFunc();
 };
-
-
+void MathFunction::PrintFunc(){
+    std::cout << "UNSET FUNCTION";
+    return;
+}
+double MathFunction::Evaluate(double val){
+    return val;
+}
 // Return false if value is not in the domain of the function, true otherwise
 bool MathFunction::ValInDomain(double val) {
     if(!restricted_domain) return true;
